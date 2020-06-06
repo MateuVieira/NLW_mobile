159baeaeea360e0eb98be0530f35d89ff9ad58e8
Mateus Vieira <mateus17v@gmail.com>
2020-06-06 15:32:55 -0300

    Update Page Points - end base code

--- src/pages/Points/index.tsx
+++ src/pages/Points/index.tsx
@@ -1,10 +1,86 @@
 import React from 'react';
-import { View } from 'react-native';
+// import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-// import { Container } from './styles';
+import {
+  Container,
+  GoBack,
+  GoBackIcon,
+  Title,
+  Description,
+  MapContainer,
+  Map,
+  MapMarker,
+  MapMarkerContainer,
+  MapMarkerImage,
+  MapMarkerTitle,
+  ItemsContainer,
+  ItemsScroll,
+  Item,
+  ItemImage,
+  ItemTitle,
+} from './styles';
 
 const Points: React.FC = () => {
-  return <View />;
+  const navigation = useNavigation();
+
+  const handleNavigationBack = () => {
+    navigation.goBack()
+  };
+
+  const handleNavigationToDetail = () => {
+    navigation.navigate('Detail')
+  }
+
+  return (
+    <>
+      <Container>
+        <GoBack onPress={handleNavigationBack} >
+          <GoBackIcon />
+        </GoBack>
+        <Title>Bem vindo.</Title>
+        <Description>Encontre no mapa um ponto de coleta.</Description>
+        <MapContainer>
+          <Map>
+            <MapMarker onPress={handleNavigationToDetail} >
+              <MapMarkerContainer>
+              <MapMarkerImage source={{uri: 'https://ei.marketwatch.com/Multimedia/2016/11/14/Photos/ZG/MW-FA143_foodpr_20161114104830_ZG.jpg?uuid=ca5a6656-aa81-11e6-95fb-001cc448aede'}} />
+              <MapMarkerTitle>Mercado</MapMarkerTitle>
+              </MapMarkerContainer>
+            </MapMarker>
+          </Map>
+        </MapContainer>
+      </Container>
+      <ItemsContainer>
+        <ItemsScroll>
+          <Item>
+            <ItemImage imageUrl={'https://weboverhauls.github.io/demos/svg/checkmark.svg'} />
+            <ItemTitle>TEste</ItemTitle>
+          </Item>
+          <Item>
+            <ItemImage imageUrl={'https://weboverhauls.github.io/demos/svg/checkmark.svg'} />
+            <ItemTitle>TEste</ItemTitle>
+          </Item>
+          <Item>
+            <ItemImage imageUrl={'https://weboverhauls.github.io/demos/svg/checkmark.svg'} />
+            <ItemTitle>TEste</ItemTitle>
+          </Item>
+          <Item>
+            <ItemImage imageUrl={'https://weboverhauls.github.io/demos/svg/checkmark.svg'} />
+            <ItemTitle>TEste</ItemTitle>
+          </Item>
+          <Item>
+            <ItemImage imageUrl={'https://weboverhauls.github.io/demos/svg/checkmark.svg'} />
+            <ItemTitle>TEste</ItemTitle>
+          </Item>
+          <Item>
+            <ItemImage imageUrl={'https://weboverhauls.github.io/demos/svg/checkmark.svg'} />
+            <ItemTitle>TEste</ItemTitle>
+          </Item>
+        </ItemsScroll>
+      </ItemsContainer>
+    </>
+  );
 }
 
 export default Points;
--- src/pages/Points/styles.tsx
+++ src/pages/Points/styles.tsx
@@ -1,5 +1,133 @@
 import styled from 'styled-components/native';
 
+import Constants from 'expo-constants';
+import { Feather } from '@expo/vector-icons';
+import MapView, { Marker } from 'react-native-maps';
+import { SvgUri } from 'react-native-svg';
+
 export const Container = styled.View`
-  
+  flex: 1;
+  padding: 0 32px;
+  padding-top: ${`${20 + Constants.statusBarHeight}px`};
 `;
+
+export const GoBack = styled.TouchableOpacity``;
+
+export const  GoBackIcon = styled(Feather).attrs({
+  name: 'arrow-left',
+  size: 20,
+  color: '#34cb79',
+})``;
+
+
+export const Title = styled.Text`
+  font-size: 20px;
+  font-family: 'Ubuntu_700Bold';
+  margin-top: 24px;
+`;
+
+export const Description = styled.Text`
+  color: #6C6C80;
+  font-size: 16px;
+  margin-top: 4px;
+  font-family: 'Roboto_400Regular';
+`;
+
+export const MapContainer = styled.View`
+  flex: 1;
+  width: 100%;
+  border-radius: 10px;
+  overflow: hidden;
+  margin-top: 16px;
+`;
+
+export const Map = styled(MapView).attrs({
+  initialRegion: {
+    latitude: -23.0216064,
+    longitude: -45.5346316,
+    latitudeDelta: 0.014,
+    longitudeDelta: 0.014,
+  },
+})`
+  width: 100%;
+  height: 100%;
+`;
+
+export const MapMarker = styled(Marker).attrs({
+  coordinate: {
+    latitude: -23.0216064,
+    longitude: -45.5346316,
+  },
+})`
+  width: 90px;
+  height: 80px; 
+`;
+
+export const MapMarkerContainer = styled.View`
+  width: 90px;
+  height: 70px;
+  background-color: #34CB79;
+  flex-direction: column;
+  border-radius: 8px;
+  overflow: hidden;
+  align-items: center;
+`;
+
+export const MapMarkerImage = styled.Image`
+  width: 90px;
+  height: 45px;
+  /* resize-mode: 'cover'; */
+`;
+
+export const MapMarkerTitle = styled.Text`
+  flex: 1;
+  font-family: 'Roboto_400Regular';
+  color: #FFF;
+  font-size: 13px;
+  line-height: 23px;
+`;
+
+export const ItemsContainer = styled.View`
+  flex-direction: row;
+  margin-top: 16px;
+  margin-bottom: 32px;
+`;
+
+export const ItemsScroll = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: {paddingHorizontal: 20},
+})``;
+
+export const Item = styled.TouchableOpacity`
+  background-color: #fff;
+  border-width: 2px;
+  border-color: #eee;
+  height: 120px;
+  width: 120px;
+  border-radius: 8px;
+  padding: 20px 16px 16px;
+  margin-right: 8px;
+  align-items: center;
+  justify-content: space-between;
+
+  text-align: center;
+`;
+
+export const ItemImage = styled(SvgUri).attrs(props => ({
+  width: 42,
+  height: 42,
+  uri: props.imageUrl,
+}))``;
+
+export const SelectedItem = styled.View`
+  border-color: #34CB79;
+  border-width: 2px;
+`;
+
+export const ItemTitle = styled.Text`
+  font-family: 'Roboto_400Regular';
+  text-align: center;
+  font-size: 13px;
+`;
+
