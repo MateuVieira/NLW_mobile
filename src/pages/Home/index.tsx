8661d3e25fc7d4853ba2220c9d33d338cc962a71
Mateus Vieira <mateus17v@gmail.com>
2020-06-06 17:58:04 -0300

    Update Page Home - update Components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, Platform } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-import { 
+import {
   Container,
-  Main, 
-  Title, 
-  Description, 
-  Button, 
+  Body,
+  Main,
+  Title,
+  Description,
+  Input,
+  Button,
   ButtonIconSpace,
-  ButtonIcon, 
-  ButtonText, 
-  Footer } from './styles';
+  ButtonIcon,
+  ButtonText,
+  Footer
+} from './styles';
 
 const Home: React.FC = () => {
   const navigation = useNavigation();
@@ -21,21 +24,33 @@ const Home: React.FC = () => {
   };
 
   return (
-  <Container  >
-    <Main>
-      <Image source={require('../../assets/logo.png')} />
-      <Title>Seu marketplace de coleta de resíduos</Title>
-      <Description>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente</Description>
-    </Main>
-    <Footer>
-      <Button onPress={handleNavigateToPoints} >
-        <ButtonIconSpace>
-          <ButtonIcon />
-        </ButtonIconSpace>
-        <ButtonText>Entrar</ButtonText>
-      </Button>
-    </Footer>
-  </Container>
+    <Container
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <Body>
+        <Main>
+          <Image source={require('../../assets/logo.png')} />
+          <View>
+            <Title>Seu marketplace de coleta de resíduos</Title>
+            <Description>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente</Description>
+          </View>
+        </Main>
+        <Footer>
+          <Input
+            placeholder="Digite a UF"
+          />
+          <Input
+            placeholder="Digite a cidade"
+          />
+          <Button onPress={handleNavigateToPoints} >
+            <ButtonIconSpace>
+              <ButtonIcon />
+            </ButtonIconSpace>
+            <ButtonText>Entrar</ButtonText>
+          </Button>
+        </Footer>
+      </Body>
+    </Container >
   );
 }
 
