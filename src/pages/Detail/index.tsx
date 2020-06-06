15f91e33d06796869d5a96c4b8dd041766dadab2
Mateus Vieira <mateus17v@gmail.com>
2020-06-06 15:55:31 -0300

    Updade Page Detail - handleNavigationBack

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 // import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { Container, GoBack, GoBackIcon, } from './styles';
 
 const Detail: React.FC = () => {
+  const navigation = useNavigation();
+
+  const handleNavigationBack = () => {
+    navigation.goBack()
+  };
+
   return (
     <Container>
-      <GoBack>
+      <GoBack onPress={handleNavigationBack} >
         <GoBackIcon />
       </GoBack>
     </Container>
